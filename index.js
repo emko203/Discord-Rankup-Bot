207089f1a70cfaca65f585c003226a1b844f2177
Emanuil Karapachov <406743@student.fontys.nl>
2021-09-18 18:20:44 +0200

    vesko changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-
+const { MessageEmbed } = require('discord.js');
 require('dotenv').config();
 
 const client = new Discord.Client();
@@ -9,23 +9,46 @@ client.once('ready', () => {
 });
 
 const replies = [
+	'',
 	'Няма да ранкъпнеш!',
 	'Не си ниска девизия заради героите, ниска девизия си щото нямаш ръце!',
 	'Спрете да ме питате тия въпроси!',
 	'Във вас е проблема, за нищо не ставате',
-	'https://images-ext-1.discordapp.net/external/tlBlI1N8VdJpNgC3HgRt5eGcb6eCB7PkQyGUhCe6fXU/%3Fcid%3D73b8f7b17e52f439a2ee49cab28f5edcd65ce966a75917e1%26rid%3Dgiphy.mp4%26ct%3Dg/https/media4.giphy.com/media/lI6jOJBGIaQwLWA9hZ/giphy.mp4',
 	'Вземете се сериозно малко.',
 	'Няма да ранкъпнеш!',
-	'Няма Jungle Gap, тебе те gap-ват.'
+	'Няма Jungle Gap, тебе те gap-ват.',
+	'Тоя сатър виждаш ли го...?'
 ];
 
 client.on('message', sendMsg);
 
 function sendMsg(msg) {
 	const dice = Math.floor(Math.random() * replies.length);
-	if (msg.channel.id === '432574891233902624' && msg.content === 'nr') {
-		msg.reply(replies[dice]);
+	const exampleEmbed = new MessageEmbed().setImage('https://media.giphy.com/media/lI6jOJBGIaQwLWA9hZ/giphy.gif');
+	if (msg.channel.id === process.env.spamCID && msg.content === '-nr') {
+		if (dice === 0) {
+			msg.reply({ embed: exampleEmbed });
+		} else {
+			msg.reply(replies[dice]);
+		}
 	}
 }
+
+// const hstucks = client.users.cache.find((user) => user.id === 'userID');
+
+const veskoID = process.env.vID;
+const repliesV = [ '', 'Няма да ранкъпнеш!', 'Героите ти не са достатъчно тежки!', 'Нямаш ръце!' ];
+
+client.on('message', function(message) {
+	const diceV = Math.floor(Math.random() * repliesV.length);
+	const heavy = new MessageEmbed().setImage('https://media.giphy.com/media/jBIinYtUd1YqG0oq8P/giphy.gif');
+	if (message.author.id === veskoID) {
+		if (diceV === 0) {
+			message.reply({ embed: heavy });
+		} else {
+			message.reply(repliesV[diceV]);
+		}
+	}
+});
 
 client.login(process.env.BOTTOKEN);
